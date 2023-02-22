e9c06b026f9cd7fc0e185a91f1d33bce26f6b314
Pamela Quartson <pamquartz31@gmail.com>
2023-02-22 18:41:34 +0000

    env file updated

--- marketplaceapp/settings.py
+++ marketplaceapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0^h)fzv&2#c8f#l_a191kg)vv@ybzkq3@1%!#*+)f#)gv1gn7e'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
